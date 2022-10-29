065042119b1ecf67776b1798e21eb0deb5f4f1f9
lersapho712570 <67900724+lersapho712570@users.noreply.github.com>
2022-10-29 16:15:30 +0700

    Update webapp.py

--- webapp.py
+++ webapp.py
@@ -1,11 +1,14 @@
 from flask import Flask, render_template,request
-import os, platform, subprocess, netifaces, random
+import os, subprocess, netifaces, random, requests
 
 
 app = Flask(__name__)
 app.config['SEND_FILE_MAX_AGE_DEFAULT'] = 0
 color = os.environ.get('COLOR')
-# color = os.environ['COLOR'] = 'red'
+env_var1 =  os.environ.get('env_var1')
+env_var2 =  os.environ.get('env_var2')
+env_var3 =  os.environ.get('env_var3')
+env_var4 =  os.environ.get('env_var4')
 
 bgcolor1 = '#63c3e9'
 bgcolor2 = '#a8d179'
@@ -16,6 +19,9 @@ bgcolor=random.choice(bgcolors)
 
 @app.route('/')
 def index():
+
+    cmdOS = """ cat /etc/os-release | grep -Ei "^NAME" | awk 'BEGIN{FS="="} {print $2}'  """
+    cmdVersion = """ cat /etc/os-release | grep -Ei "^version_id" | awk 'BEGIN{FS="="} {print $2}'  """
 
     gateways = netifaces.gateways()
     gateway = gateways['default'][2][0]
@@ -44,20 +50,18 @@ def index():
         mount = False
         texts = 'volume not mount'
 
-
-   
     var1 = {
         'name': hostname,
         'ip': ip,
         'netmask': netmask,
         'gateway': gateway,
         'mount': mount,
-        'os-base': platform.linux_distribution()[0],
-        'os-version': platform.linux_distribution()[1],
+        'os-base': subprocess.check_output(cmdOS,shell=True).decode("utf-8").split("\n")[0],
+        'os-version': subprocess.check_output(cmdVersion,shell=True).decode("utf-8").split("\n")[0]
     }
 
 
-    return render_template('webapp.html', page='Index', var=var1, text=texts, color=color, bgcolor=bgcolor)
+    return render_template('webapp.html', page='Index', var=var1, text=texts, color=color,env_var1=env_var1,env_var2=env_var2,env_var3=env_var3,env_var4=env_var4,bgcolor=bgcolor)
 
 
 if __name__ == '__main__':
